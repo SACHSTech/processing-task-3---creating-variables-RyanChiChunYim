9cc9f54f651115b9b4c726bf0bb9855fdba3af1d
Ryan  Yim <75390133+RyanChiChunYim@users.noreply.github.com>
2021-12-16 01:20:59 +0000

    better spacing

--- Sketch.java
+++ Sketch.java
@@ -7,11 +7,11 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution, put your size all in this method
    */
 
-   int cx, cy;
-   float secondsRadius;
-   float minutesRadius;
-   float hoursRadius;
-   float clockDiameter;
+  int cx, cy;
+  float secondsRadius;
+  float minutesRadius;
+  float hoursRadius;
+  float clockDiameter;
   public void settings() {
 	// put your size call here
     size(400, 400);
@@ -29,8 +29,8 @@ public class Sketch extends PApplet {
    hoursRadius = radius * 1;
    clockDiameter = radius * 2;
   
-  cx = width / 2;
-  cy = height / 2;
+    cx = width / 2;
+    cy = height / 2;
   }
 
   /**
@@ -51,13 +51,12 @@ public class Sketch extends PApplet {
   noStroke();
   ellipse(cx, cy, clockDiameter, clockDiameter);
   
-  // Angles for sin() and cos() start at 3 o'clock;
-  // subtract HALF_PI to make them start at the top
+
   float s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
   float m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI; 
   float h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
   
-  // Draw the hands of the clock
+
   stroke(255);
   strokeWeight(1);
   line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
@@ -66,7 +65,7 @@ public class Sketch extends PApplet {
   strokeWeight(4);
   line(cx, cy, cx + cos(h) * hoursRadius, cy + sin(h) * hoursRadius);
   
-  // Draw the minute ticks
+
   strokeWeight(2);
   beginShape(POINTS);
   for (int a = 0; a < 360; a+=6) {
@@ -76,11 +75,6 @@ public class Sketch extends PApplet {
     vertex(x, y);
   }
   endShape();
-
-
-
-
-
 
   }
   
